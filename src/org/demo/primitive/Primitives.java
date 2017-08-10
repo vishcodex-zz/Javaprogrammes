18e71fc76e8cbe6801962cb5779ab1c5c4ca5529
git test <vishnu.mits@gmail.com>
2017-08-10 15:28:32 -0400

    interger

--- src/org/demo/primitive/Primitives.java
+++ src/org/demo/primitive/Primitives.java
@@ -3,11 +3,13 @@ package org.demo.primitive;
 public class Primitives 
 {
 	static int i;
+	static Integer i1;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		System.out.println(i1);
 		
 		System.out.println(i);
+		
 	}
 
 }
