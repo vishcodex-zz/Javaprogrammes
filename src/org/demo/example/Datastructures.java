cb42038049d39f6db36ee2b7a73201fc82f37594
git test <vishnu.mits@gmail.com>
2017-08-10 13:42:05 -0400

    added

--- src/org/demo/example/Datastructures.java
+++ src/org/demo/example/Datastructures.java
@@ -2,9 +2,21 @@ package org.demo.example;
 
 public class Datastructures {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-//comment
+		char[] chararray = new char[5];
+		
+		chararray[0] = 'H';
+		chararray[1] = 'E';
+		chararray[2] = 'L';
+		chararray[3] = 'L';
+		chararray[4] = 'O';
+		
+		for(int i=0; i<chararray.length;i++) 
+		{
+			System.out.println(chararray[i]);
+		}
 	}
 
 }
