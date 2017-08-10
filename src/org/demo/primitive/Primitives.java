d70d9bcd96408e20356df3de49e9b0899a724728
git test <vishnu.mits@gmail.com>
2017-08-10 15:04:21 -0400

    primitives

---
+++ src/org/demo/primitive/Primitives.java
@@ -0,0 +1,13 @@
+package org.demo.primitive;
+
+public class Primitives 
+{
+	static int i;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		System.out.println(i);
+	}
+
+}
